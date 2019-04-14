02b923b85597c296b9c3114b1a49c5af92959822
naokiur <atomic.transfer.comp@gmail.com>
2019-04-14 16:57:07 +0900

    [feat]Add BootstrapVue and Enabled.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
+    <Container></Container>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+  import { Component, Vue } from 'vue-property-decorator';
+  import Container from '@/components/Container.vue';
 
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      Container,
+    },
+  })
+  export default class Home extends Vue {}
 </script>
