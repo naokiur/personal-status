0995706238f00917b6ce2d4afafd61ab1b7965af
naokiur <atomic.transfer.comp@gmail.com>
2019-04-17 18:40:09 +0900

    [feat]Change to HolizontalBar.

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,11 +1,11 @@
 <script lang="ts">
-import { Bar } from 'vue-chartjs';
+import { HorizontalBar } from 'vue-chartjs';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
-    extends: Bar, // this is important to add the functionality to your component
+    extends: HorizontalBar, // this is important to add the functionality to your component
 })
-export default class Chart extends Vue<Bar> {
+export default class Chart extends Vue<HorizontalBar> {
   mounted (): void {
     // Overwriting base render method with actual data.
     this.renderChart({
