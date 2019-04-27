0050d5e43ac0702e2655fb7d5f3b18d542bd7f7e
naokiur <atomic.transfer.comp@gmail.com>
2019-04-27 13:33:07 +0900

    [WIP]Try to add dynamic textarea for classification.

--- src/components/StatusInput.vue
+++ src/components/StatusInput.vue
@@ -1,20 +1,43 @@
 <template>
-  <div>
-      入力画面
-
-  </div>
+  <b-container>
+    <b-row>
+      <b-col>
+        <b-form-textarea
+          v-model="major"
+          rows=6
+          max-rows="6"
+          placeholder="input 区分"
+        >
+        </b-form-textarea>
+      </b-col>
+      <b-col>
+        <b-row v-for="majorData in dataList" :key="majorData">
+          <b-form-group
+            :id="`group-${majorData}`"
+            :label="majorData"
+            :label-for="majorData"
+          >
+            <b-form-textarea
+              :id="majorData"
+              rows="3"
+              max-rows="3"
+              :placeholder="`input ${majorData}`"
+            >
+            </b-form-textarea>
+          </b-form-group>
+        </b-row>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
-<script lang="ts">
-  import { Vue } from 'vue-property-decorator';
-//   import Avatar from '@/components/Avatar.vue';
-//   import Status from '@/components/Status.vue';
-
-//   @Component({
-//       components: {
-//           Avatar,
-//           Status,
-//       },
-//   })
-  export default class StatusInput extends Vue {}
+<script>
+export default {
+  data() {
+    return {
+      major: '',
+      dataList: ['a', 'b']
+    }
+  }
+}
 </script>
