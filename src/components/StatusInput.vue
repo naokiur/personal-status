5caadcf530a1ea8d6934b3c218e3bb4ef34e4ad1
naokiur <atomic.transfer.comp@gmail.com>
2019-04-27 15:47:29 +0900

    [feat]generate data for chart.

--- src/components/StatusInput.vue
+++ src/components/StatusInput.vue
@@ -20,35 +20,65 @@
       <b-col>
         <b-row v-for="category in generateCategories" :key="category">
           <b-form-group
-            :id="`group-${category}`"
-            :label="category"
-            :label-for="category"
+            :id="`group-${category.id}`"
+            :label="category.id"
+            :label-for="category.id"
           >
             <b-form-textarea
-              :id="category"
+              :id="category.id"
+              v-model="category.smallCategoryInput"
               rows="3"
               max-rows="3"
-              :placeholder="`input ${category}`"
+              :placeholder="`input ${category.id}`"
             >
             </b-form-textarea>
           </b-form-group>
         </b-row>
       </b-col>
     </b-row>
+    <b-row v-for="category in this.cateList" :key="category">
+      <b-row>
+        <b-col>
+          {{ category.id }}
+        </b-col>
+        <b-col>
+          {{ category.smallCategoryInput }}
+        </b-col>
+      </b-row>
+    </b-row>
   </b-container>
 </template>
 
 <script>
 export default {
+
   data() {
     return {
-      categoryInput: ''
+      categoryInput: '',
+      cateList: []
     }
   },
   computed: {
     generateCategories() {
-      const cateList = this.categoryInput.split('\n')
-      return cateList.filter(v => v != '')
+      const inputList = this.categoryInput.split('\n')
+        // Ignore blank data.
+        .filter(v => v != '')
+      
+      this.cateList = inputList.map(v => {
+        return {
+          id: v,
+          smallCategoryInput: '',
+          smallCategory: [
+            {
+              id: '',
+              level: 0
+            }
+          ]
+        }
+      })
+
+      
+      return this.cateList
     }
   }
 }
