4a9afa7daeef062fa13b42ee1b0175402b0c13b2
naokiur <atomic.transfer.comp@gmail.com>
2019-04-29 16:32:58 +0900

    [feat]Create Component

---
+++ src/components/SmallCategory.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-form-textarea
+        :id="category.id"
+        v-model="smallCategoryInput"
+        rows="3"
+        max-rows="3"
+        :placeholder="`input ${category.name}`"
+    >
+    </b-form-textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        smallCategoryInput: '',
+        smallCategoryList: []
+      }
+  },
+  props: {
+    category: {
+      type: Object,
+      default() {
+        return {
+          id: '',
+          name: ''
+        }
+      }
+    }
+  },
+  watch: {
+    smallCategoryInput(newInput, oldInput) {
+      this.smallCategoryList = newInput.split('\n').filter(v => v != '')
+      console.log(this.smallCategoryList.length)
+    } 
+  }
+  // computed: {
+  //   smallCategoryInput: {
+  //     // get() {
+  //     //   return this.smallCategoryInput
+  //     // },
+  //     set(newInput) {
+  //       this.smallCategoryInput = newInput
+  //       this.smallCategoryList = newInput.split('\n').filter(v => v != '')
+  //     }
+  //   }
+  // }
+}
+</script>
--- src/components/StatusInput.vue
+++ src/components/StatusInput.vue
@@ -13,9 +13,6 @@
             rows=6
             max-rows="6"
             placeholder="input category"
-            @keypress="generateCategoryList"
-            @keydown="generateCategoryList"
-            @keyup="generateCategoryList"
           >
           </b-form-textarea>
         </b-form-group>
@@ -27,17 +24,7 @@
             :label="category.name"
             :label-for="category.id"
           >
-            <b-form-textarea
-              :id="category.id"
-              v-model="category.smallCategoryInput"
-              rows="3"
-              max-rows="3"
-              :placeholder="`input ${category.name}`"
-              @keypress="generateSmallCategoryList(category.id)"
-              @keydown="generateSmallCategoryList(category.id)"
-              @keyup="generateSmallCategoryList(category.id)"
-            >
-            </b-form-textarea>
+            <SmallCategory :category="category"></SmallCategory>
           </b-form-group>
         </b-row>
         <!-- <b-row v-for="category in generateSmallCategories" :key="category">
@@ -58,86 +45,82 @@
         </b-row> -->
       </b-col>
     </b-row>
-    <b-row v-for="category in categoryList" :key="category.id">
+    <!-- <b-row v-for="category in categoryList" :key="category.id">
       <b-col>
         <b-row v-for="smallCategory in category.smallCategoryList" :key="smallCategory.id">
           <b-form-group>
-            <p>{{ `${category.name} : ${smallCategory.name} : ${smallCategory.level}`  }}</p>
+            <p>{{ `${category.name} : ${smallCategory}`  }}</p>
           </b-form-group>
         </b-row>
       </b-col>
-    </b-row>
+    </b-row> -->
   </b-container>
 </template>
 
 <script>
+import SmallCategory from '@/components/SmallCategory.vue'
+
 export default {
   data() {
     return {
       categoryInput: '',
-      categoryList: [],
       smallCategoryList: []
     }
-  // },
-  // computed: {
-  //   generateSmallCategories() {
-  //     const inputList = this.categoryInput.split('\n')
-  //       // Ignore blank data.
-  //       .filter(v => v != '')
-      
-  //     this.cateList = inputList.map(v => {
-  //       return {
-  //         id: v,
-  //         smallCategoryInput: '',
-  //         smallCategory: [
-  //           {
-  //             id: '',
-  //             level: 0
-  //           }
-  //         ]
-  //       }
-  //     })
-
-  //     return inputList
-  //   },
   },
-  methods: {
-  //   generateSmallCategory(id) {
-  //     const smallInput = this.cateList.filter(v => v.id = id)[0].smallCategoryInput
-  //     this.cateList.filter(v => v.id = id)[0].smallCateList = smallInput.split(`\n`).filter(v => v != '').map(v => {
-  //       return {
-  //         id: v,
-  //         level: 0
-  //       }
-  //     })
-  //   }
-    generateCategoryList() {
-      this.categoryList = this.categoryInput.split('\n')
+  computed: {
+    categoryList() {
+      return this.categoryInput.split('\n')
         // Ignore blank data.
         .filter(v => v != '').map((v, index) => {
           return {
             id: `id-${index}`,
             name: v,
-            smallCategoryInput: '',
-            smallCategoryList: []
+            // smallCategoryInput: '',
+            // smallCategoryList: [1, 2, 3]
           }
-        })
-    },
-    generateSmallCategoryList(categoryId) {
-      const targetCategory = this.categoryList.filter(v => v.id == categoryId)[0]
-      targetCategory.smallCategoryList 
-        = targetCategory.smallCategoryInput
-          .split('\n')
-          .filter(v => v != '')
-          .map((v, index) => {
-            return {
-              id: `small-id-${index}`,
-              name: v,
-              level: 0,
-              category: categoryId
-            }
-          })
+      })
     }
+  },
+  components: {
+    SmallCategory
   }
+  // methods: {
+  // //   generateSmallCategory(id) {
+  // //     const smallInput = this.cateList.filter(v => v.id = id)[0].smallCategoryInput
+  // //     this.cateList.filter(v => v.id = id)[0].smallCateList = smallInput.split(`\n`).filter(v => v != '').map(v => {
+  // //       return {
+  // //         id: v,
+  // //         level: 0
+  // //       }
+  // //     })
+  // //   }
+  //   generateCategoryList() {
+  //     this.categoryList = this.categoryInput.split('\n')
+  //       // Ignore blank data.
+  //       .filter(v => v != '').map((v, index) => {
+  //         return {
+  //           id: `id-${index}`,
+  //           name: v,
+  //           smallCategoryInput: '',
+  //           smallCategoryList: []
+  //         }
+  //       })
+  //   },
+  //   generateSmallCategoryList(categoryId) {
+  //     const targetCategory = this.categoryList.filter(v => v.id == categoryId)[0]
+  //     targetCategory.smallCategoryList 
+  //       = targetCategory.smallCategoryInput
+  //         .split('\n')
+  //         .filter(v => v != '')
+  //         .map((v, index) => {
+  //           return {
+  //             id: `small-id-${index}`,
+  //             name: v,
+  //             level: 0,
+  //             category: categoryId
+  //           }
+  //         })
+  //   }
+  // }
 }
 </script>
